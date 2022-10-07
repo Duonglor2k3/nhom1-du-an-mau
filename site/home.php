5f73fd9c2de52998b00436652fdfc4a9ad0f56e8
vuphuthi <114985279+vuphuthi@users.noreply.github.com>
2022-10-08 01:04:56 +0700

    thivp-update-giao-dien-home

---
+++ site/home.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Anton&family=Inter:wght@700&family=Roboto+Slab:wght@400;500&family=Roboto:ital@1&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="http://localhost/nhom1-du-an-mau/content/css/detail.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <section class="header__banner">
+                <div class="header__banner__introduce">
+                    <div class="">
+                        <h1>Shop đá Bóng Số 1 Hà Nội</h1>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="/nhom1-du-an-mau/site/index.php">Trang chủ</a></li>
+                            <li><a href="/nhom1-du-an-mau/site/index.php?gioi-thieu">Giới thiệu</a></li>
+                            <li><a href="/nhom1-du-an-mau/site/index.php?ao-nam">Áo nam</a></li>
+                            <li><a href="/nhom1-du-an-mau/site/index.php?ao-nu">Áo Nữ</a></li>
+                            <li><a href="">Blog</a></li>
+                            <li><a href="">Quy Trình</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
+            <section>
+                <div class="header__banner__logo">
+                    <div class="header__banner__logo__img">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/logo.png" alt="">
+                    </div>
+                    <div class="header__banner__logo__form">
+                        <form action="" class="search" method="post">
+                            <input type="text" class="searchTerm" placeholder="Bạn muốn tìm gì?">
+                        </form>
+                        <a href=""><img src="http://localhost/nhom1-du-an-mau/content/img/search.png" alt=""></a>
+                    </div>
+                    <div class="header__banner__logo__time">
+                        <p>9:00 - 17:00</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="header__banner__menu">
+                    <nav>
+                        <ul>
+                        <li><a href="/nhom1-du-an-mau/site/index.php">Trang chủ</a></li>
+                            <li><a href="/nhom1-du-an-mau/site/index.php?gioi-thieu">Giới thiệu</a></li>
+                            <li><a href="/nhom1-du-an-mau/site/index.php?ao-nam">Áo nam</a></li>
+                            <li><a href="/nhom1-du-an-mau/site/index.php?ao-nu">Áo Nữ</a></li>
+                            <li><a href="#">Áo MU</a></li>
+                            <li><a href="#">Liên Hệ</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="banner">
+                    <img src="http://localhost/nhom1-du-an-mau/content/img/banner.png" alt="">
+                </div>
+            </section>
+        </header>
+        <main>
+            <section>
+                <div class="tittle">
+                    <span>Sản Phẩm</span>
+                </div>
+            </section>
+
+            <section>
+                <div class="main_content">
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp2.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp4.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp5.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp3.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp2.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp4.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp5.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+
+                    <div class="content">
+                        <img src="http://localhost/nhom1-du-an-mau/content/img/sp3.png" alt="">
+                        <p>Áo Không Logo HPL-Nikita Mới Nhất 2022 Màu Xanh Ya</p>
+                        <span>giá : 200.000 </span>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+        <footer>
+            <section>
+                <div class="footer">
+                    <section>
+                        <div class="content_footer">
+                            <div class="description">
+                                <span>GIỚI THIỆU VỀ HPL SPORT</span>
+                                <p>HPL Sport là một trong những đơn vị tiên phong trong lĩnh vực thiết kế và sản xuất
+                                    quần
+                                    áo
+                                    thi đấu bóng đá theo yêu cầu với quy mô và cách làm chuyên nghiệp. Hãy đến ngay với
+                                    chúng
+                                    tôi để cảm nhận sự khác biệt.</p>
+                            </div>
+
+                            <div class="information">
+                                <span>LIÊN HỆ VỚI CHÚNG TÔI</span>
+
+                                <section>
+                                    <div class="name">
+                                        <p>Shop Bóng Đá Phủi Số 1 Hà Nội - HPL SPORT</p>
+                                    </div>
+                                </section>
+
+                                <section>
+                                    <div class="dia_chi">
+                                        <p>Shop 1 : Phú Đô, Mỹ Đình, Hà Nội</p>
+                                        <p>Shop 2 : Số 89, Ngõ 345 Khương Trung, Thanh Xuân, Hà Nội</p>
+                                        <p>Hotline: 0976.234.272 (Có Zalo, Facebook)</p>
+                                    </div>
+                                </section>
+
+                                <section>
+                                    <div class="descrip">
+                                        <p>Chúng tôi cung cấp nhiều dòng vải để khách hàng có nhiều lựa chọn từ rẻ tiền
+                                            đến vải cao cấp nhất trong lĩnh vực quần áo bóng đá.</p>
+                                    </div>
+                                </section>
+
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="pay">
+                            <a href="#"><img src=".http://localhost/nhom1-du-an-mau/content/img/pay/Frame.png" alt=""></a>
+                            <a href="#"><img src=".http://localhost/nhom1-du-an-mau/content/img/pay/Frame.png" alt=""></a>
+                            <a href="#"><img src=".http://localhost/nhom1-du-an-mau/content/img/pay/stripe.png" alt=""></a>
+                            <a href="#"><img src=".http://localhost/nhom1-du-an-mau/content/img/pay/card.png" alt=""></a>
+                            <a href="#"><img src=".http://localhost/nhom1-du-an-mau/content/img/pay/cashon.png" alt=""></a>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="mail">
+                            <p>Copyright 2022 © HPL SPORT</p>
+                        </div>
+                    </section>
+                </div>
+            </section>
+        </footer>
+    </div>
+</body>
+
+</html>
--- site/index.php
+++ site/index.php
@@ -1,4 +1,5 @@
 <?php 
+<<<<<<< Updated upstream
 if(isset($_GET['product'])){
 
 $VIEW_NAME = 'product.php';
@@ -32,3 +33,28 @@ $VIEW_NAME = 'home.php';
 
 include_once './layout.php';
 ?>
+=======
+    if(isset($_GET['san-pham'])){
+        $VIEWNAME = 'san-pham.php';
+    } else if (isset($_GET['gio-hang'])){
+        $VIEWNAME = 'gio-hang.php';
+    }else if (isset($_GET['chi-tiet'])){
+        $VIEWNAME = 'chi-tiet.php';
+    }
+    else if (isset($_GET['gioi-thieu'])){
+        $VIEWNAME = 'gioi-thieu.php';
+    }
+    else if (isset($_GET['ao-nam'])){
+        $VIEWNAME = 'ao-nam.php';
+    }
+    else if (isset($_GET['ao-nu'])){
+        $VIEWNAME = 'ao-nu.php';
+    } 
+    else {
+        $VIEWNAME = 'home.php';
+    }
+
+    include_once "./layout.php"
+
+?>
+>>>>>>> Stashed changes
